c6d631f67bb1beba0e4f52d903a4c262b8890561
unknown <dshipuk@yahoo.com>
2022-05-27 11:02:23 -0700

    Restricted now checks if Auth Token is valid and responds with correct messages if not. You can get jokes api data if u provide a valid auth token in authorization header

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -75,7 +75,7 @@ router.post('/login', validateLogin, usernameValid,  (req, res) => {
 
 function generateToken(user) {
   const payload = {
-    subject: user.username,
+    subject: user.id,
     username: user.username
   }
   const options = {
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const { JWT_SECRET } = require("../../secrets/index")
+const jwt = require("jsonwebtoken")
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+
+  if(!token) {
+    return res.status(401).json("token required")
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json("token invalid")
+    } else {
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
   /*
     IMPLEMENT
 
